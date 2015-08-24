3618fdb48fbac93d728e4749c43d7df63906fa90
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2015-08-23 20:16:40 -0700

    Initial Upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Space-Invaders
+This is the repository for the Space Invaders Demo I will have at Club Rush
---
+++ desktop/src/com/tcg/spaceinvaders/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.tcg.spaceinvaders.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tcg.spaceinvaders.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
