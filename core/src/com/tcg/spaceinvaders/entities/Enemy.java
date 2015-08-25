f6014e9788743e5383e062a2b413cf11b7865f76
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2015-08-24 19:38:08 -0700

    #9 Created Enemy

---
+++ core/src/com/tcg/spaceinvaders/entities/Enemy.java
@@ -0,0 +1,62 @@
+package com.tcg.spaceinvaders.entities;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.tcg.spaceinvaders.MyConstants;
+
+public class Enemy extends Entity {
+
+	private Texture texDown;
+	private Texture texUp;
+	
+	public static final float width = 12, height = 8;
+	
+	private boolean up;
+	
+	public Enemy(int row, int col) {
+		super();
+		String path = "entities/enemies/";
+		if(row < 2) {
+			texDown = new Texture(path + "bottom0.png");
+			texUp = new Texture(path + "bottom1.png");
+		} else if(row < 4) {
+			texDown = new Texture(path + "center0.png");
+			texUp = new Texture(path + "center1.png");
+		} else {
+			texDown = new Texture(path + "top0.png");
+			texUp = new Texture(path + "top1.png");
+		}
+		bounds.set((MyConstants.GAME_WIDTH * .1355f) + (col * (width * 1.5f)) + ((width - texDown.getWidth()) * .5f), (MyConstants.WORLD_HEIGHT * .5f) + (row * (height * 2.5f)), texDown.getWidth(), texDown.getHeight());
+		vel.set(2.5f, 0);
+	}
+
+	public void update(boolean move, boolean up) {
+		this.up = up;
+		if(move) {
+			bounds.x += vel.x;
+		}
+	}
+	
+	@Override
+	public void draw(ShapeRenderer sr, SpriteBatch sb, float dt) {
+		if(this.up) {
+			sb.draw(texUp, getX(), getY());
+		} else {
+			sb.draw(texDown, getX(), getY());
+		}
+	}
+	
+	public void bounceVelocity() {
+		vel.x *= -1;
+		bounds.x += vel.x;
+		bounds.y -= 2;
+	}
+
+	@Override
+	public void dispose() {
+		texDown.dispose();
+		texUp.dispose();
+	}
+
+}
--- core/src/com/tcg/spaceinvaders/gamestates/PlayState.java
+++ core/src/com/tcg/spaceinvaders/gamestates/PlayState.java
@@ -22,6 +22,12 @@ public class PlayState extends GameState {
 	
 	private Array<Shield> shields;
 	
+	private Array<Enemy> enemies;
+	
+	private float moveTime, moveTimer;
+	
+	private boolean enemyUp;
+	
 	public PlayState(GameStateManager gsm) {
 		super(gsm);
 	}
@@ -42,12 +48,24 @@ public class PlayState extends GameState {
 		shields.add(new Shield((MyConstants.GAME_WIDTH * .5f) + (Shield.totalWidth * .5f), MyConstants.WORLD_HEIGHT * .15f - (Shield.totalHeight * .5f)));
 		shields.add(new Shield((MyConstants.GAME_WIDTH * .5f) + (Shield.totalWidth * 2) + (Shield.totalWidth * .5f), MyConstants.WORLD_HEIGHT * .15f - (Shield.totalHeight * .5f)));
 		shields.add(new Shield((MyConstants.GAME_WIDTH * .5f) + (Shield.totalWidth * 4) + (Shield.totalWidth * .5f), MyConstants.WORLD_HEIGHT * .15f - (Shield.totalHeight * .5f)));
+	
+		enemies = new Array<Enemy>();
+		for(int row = 0; row < 5; row++) {
+			for(int col = 0; col < 11; col++) {
+				enemies.add(new Enemy(row, col));
+			}
+		}
+		
+		moveTimer = .5f;
+		moveTime = 0;
+		
+		enemyUp = false;
+		
 	}
 
 	@Override
 	public void handleInput() {
 		p.handleInput();
-
 	}
 
 	@Override
@@ -55,6 +73,23 @@ public class PlayState extends GameState {
 		p.update();
 		for(Shield s : shields) {
 			s.update();
+		}
+		
+		moveTime+= dt;
+		for(Enemy e : enemies) {
+			e.update(moveTime >= moveTimer, enemyUp);
+		}
+		if(moveTime >= moveTimer) {
+			enemyUp = !enemyUp;
+			moveTime = 0;
+		}
+		for(Enemy e : enemies) {
+			if(e.getRight() >= MyConstants.GAME_WIDTH || e.getX() <= 0) {
+				for(Enemy e1 : enemies) {
+					e1.bounceVelocity();
+				}
+				break;
+			}
 		}
 	}
 
@@ -71,6 +106,9 @@ public class PlayState extends GameState {
 		
 		sb.begin();
 		sb.setProjectionMatrix(cam.combined);
+		for(Enemy e : enemies) {
+			e.draw(sr, sb, dt);
+		}
 		p.draw(sr, sb, dt);
 		sb.end();
 
@@ -96,8 +134,10 @@ public class PlayState extends GameState {
 
 	@Override
 	public void dispose() {
-		// TODO Auto-generated method stub
-
+		p.dispose();
+		for(Enemy e : enemies) {
+			e.dispose();
+		}
 	}
 
 }
